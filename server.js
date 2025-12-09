60f4a389f5e936ebc0ee537614898fbf110cb131
Sunil Kumar <sunilkumar@sharviinfotech.com>
2025-12-09 10:37:19 +0530

    nw server,js file changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
@@ -7,13 +8,13 @@ const connectDB = require('./config/db');
 const app = express();
 
 // Middleware
-// app.use(cors());
-// for deve server
-app.use(cors({
-    origin: '*', // Adjust the origin to your frontend's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-}));
+app.use(cors());
+//for deve server
+// app.use(cors({
+//     origin: 'http://10.10.4.178', // Adjust the origin to your frontend's URL
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+// }));
 app.use(express.json());
 
 // Load Routes
@@ -22,11 +23,56 @@ app.use('/api', routes);
 // Database Connection
 // connectDB();
 
-// Server Initialization
-// const PORT = process.env.PORT || 3000;
+// local code
 const BASE_SERVER_URL = 'http://localhost'
-const BASE_PORT = 3000
+const BASE_PORT = 3001
 app.listen(BASE_PORT, () => 
     // console.log(`Server running on port ${PORT}`)
  console.log(`🚀 Server running on ${BASE_SERVER_URL}:${BASE_PORT}`)
 );
+
+
+
+// Server code 
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// const routes = require('./routes');
+// const connectDB = require('./config/db');
+
+// // Initialize App
+// const app = express();
+
+// // Middleware
+// // app.use(cors());
+// // for deve server
+// app.use(cors({
+//     origin: '*', // Adjust the origin to your frontend's URL
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+// }));
+// app.use(express.json());
+
+// // Load Routes
+// app.use('/api', routes);
+
+// // Database Connection
+// // connectDB();
+
+// // Server Initialization
+// // const PORT = process.env.PORT || 3000;
+// const BASE_SERVER_URL = 'http://localhost'
+// const BASE_PORT = 3001
+// app.listen(BASE_PORT, () => 
+//     // console.log(`Server running on port ${PORT}`)
+//  console.log(`🚀 Server running on ${BASE_SERVER_URL}:${BASE_PORT}`)
+// );
+
+
+
